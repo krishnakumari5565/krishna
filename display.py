5727caafbfc1e700bab6542bd7a10f6a046dddf6
krishnakumari5565 <118648557+krishnakumari5565@users.noreply.github.com>
2022-11-20 10:22:11 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,8 @@
+list=[10,20,30,40,50]
+rest_list=[x*2 for x in list]
+print(rest_list)
+
+list1=[100,200,300,400,500]
+rest_list2=[x*0.1 for x in list1]
+print(rest_list2)
+print("the list is completed")
